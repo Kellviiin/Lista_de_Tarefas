ee690f978cf11fda6ec0c113dd45807d2b5ce9d2
Kelvin <kelabrantes.dev@gmail.com>
2023-01-28 15:59:48 -0300

    background

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function criar(){
 let p = document.createElement('div')
 let input = document.querySelector('input')
 let main = document.querySelector('main')
-p.textContent = input.value
-p.style.border = "1px solid red" 
+p.textContent = `Você tem que ${input.value}` 
+p.style.backgroundColor = "white"
 p.style.height = "20px"
 p.style.display = "flex"
 p.style.justifyContent = "center"
